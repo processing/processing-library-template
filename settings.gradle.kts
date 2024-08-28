c390068f7cec12af7445ed39894e65a2907fe6b6
mingness <mingness@users.noreply.github.com>
2024-08-28 11:56:28 +0200

    improve comments for beginner understanding (#42)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
-rootProject.name = "mylibrary"
+// the short name of your library. This string will name relevant files and folders.
+// Such as:
+// <rootProject.name>.jar will be the name of your build jar
+// <rootProject.name>.zip will be the name of your release file
+rootProject.name = "myLibrary"
